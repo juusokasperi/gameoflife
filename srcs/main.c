a4bc8d6321f56c180e59a1568ebf19ed54971801
juusokasperi <juusor@gmail.com>
2025-07-31 16:42:22 +0300

    Separated the basic validation into it's own function

--- srcs/main.c
+++ srcs/main.c
@@ -6,31 +6,43 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 11:59:18 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/07/31 16:02:08 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/07/31 16:38:21 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_of_life.h"
 
-static void	prepare_state(t_state *state, const char *initial_state);
+static void	prepare_state(t_state *state, int32_t fd);
 static void	elapsed_time(struct timeval *start, struct timeval *end);
+static void	basic_validation(int argc, char **argv, int32_t *fd, int32_t *iterations);
 
 int	main(int argc, char **argv)
 {
 	t_state	state;
 	int32_t	iterations;
+	int32_t	fd;
 	struct timeval start, end;
 
-	if (argc != 3)
-		invalid_format(argv[0]);
-	iterations = check_valid_iterations(argv[2]);
+	basic_validation(argc, argv, &fd, &iterations);
 	gettimeofday(&start, NULL);
-	prepare_state(&state, argv[1]);
+	prepare_state(&state, fd);
 	play_game(&state, iterations);
 	print_state(&state);
 	free_array(&state);
 	gettimeofday(&end, NULL);
 	elapsed_time(&start, &end);
+}
+
+/*
+	Checks that the program is called with right amount of arguments,
+	the iterations is a valid integer and the initial_state is a readable file.
+*/
+static void	basic_validation(int argc, char **argv, int32_t *fd, int32_t *iterations)
+{
+	if (argc != 3)
+		invalid_format(argv[0]);
+	*iterations = check_valid_iterations(argv[2]);
+	*fd = check_file_open(argv[1]);
 }
 
 static void	elapsed_time(struct timeval *start, struct timeval *end)
@@ -43,13 +55,12 @@ static void	elapsed_time(struct timeval *start, struct timeval *end)
 	}
 	printf("Time elapsed: %ld.%06lds\n", seconds, microseconds);
 }
+
 /*
 	Prepares the struct for the game.
 */
-static void	prepare_state(t_state *state, const char *initial_state)
+static void	prepare_state(t_state *state, int32_t fd)
 {
-	int32_t	fd = check_file_open(initial_state);
-
 	check_size_and_validness(state, fd);
 	allocate_memory(state, fd);
 	initial_to_struct(state, fd);
